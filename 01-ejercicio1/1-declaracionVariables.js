17b8cbd2a1fedba38dac4d7f8791ef0e8e907bc0
505 <505@academicos.cesde.edu.co>
2020-02-10 20:19:31 -0500

    let y var

---
+++ 01-ejercicio1/1-declaracionVariables.js
@@ -0,0 +1,20 @@
+/*var nombre = "juan";
+var nombre = "camilo";
+console.log(nombre);
+*/
+
+let costo = 5 ;
+if(costo > 3){
+    var comida = "galletas";
+}
+console.log("La comida: " + comida);
+
+for(var i=0; i<5; i++){
+
+}
+console.log(i);
+
+for(let i=0; i<5; i++){
+
+}
+console.log(i);
