f3eca7949949b35266c5daced761cac1bc26956e
505 <505@academicos.cesde.edu.co>
2020-02-24 20:29:56 -0500

    Flex box class

--- 04-clase-07_02_2020/01-components/App.js
+++ 04-clase-07_02_2020/01-components/App.js
@@ -3,17 +3,26 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.box1}>
-        <Text style={styles.text}>Vista 1</Text>
+    <>
+      <View style={styles.container}>
+        <View style={styles.box1}>
+          <Text style={styles.text}>Vista 1</Text>
+          <Text style={styles.text}>Vista 1</Text>
+          <Text style={styles.text}>Vista 1</Text>
+        </View>
+        <View style={styles.box2}>
+          <Text style={styles.text}>Vista 2</Text>
+        </View>
       </View>
-      <View style={styles.box2}>
-        <Text style={styles.text}>Vista 2</Text>
+      <View style={styles.container2}>
+        <View style={styles.box3}>
+          <Text style={styles.text}>Vista 3</Text>
+        </View>
+        <View style={styles.box4}>
+        <Text style={styles.text}>Vista 4</Text>
       </View>
-      <View style={styles.box3}>
-        <Text style={styles.text}>Vista 3</Text>
       </View>
-    </View>
+    </>
   );
 }
 
@@ -21,14 +30,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#311b92',
-    flexDirection: 'column'
+    flexDirection: 'row',
+    
+  },
+  container2: {
+    flex: 1,
+    backgroundColor: '#311b92',
+    flexDirection: 'row',
+    
   },
   text: {
     color:'#2e7d32',
   },
   box1: {
-    flex :3,
-    backgroundColor: '#f57f17'
+    flex :1,
+    backgroundColor: '#f57f17',
+    flexDirection: 'row'
+     
+    
   },
   box2: {
     flex :1,
@@ -37,5 +56,9 @@ const styles = StyleSheet.create({
   box3: {
     flex :1,
     backgroundColor: '#ff1744'
+  },
+  box4: {
+    flex :1,
+    backgroundColor: 'blue'
   }
 });
